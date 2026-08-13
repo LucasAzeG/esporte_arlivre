f605f30ede33c5f900caeb1eee42248262deea38
Lucas Azevedo <lucas.a.goes@aluno.senai.br>
2026-08-13 14:42:32 -0300

    Criando os Componentes

---
+++ EsporteArlivre/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponentComponent } from './component/menu-component/menu-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MenuComponentComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+}
